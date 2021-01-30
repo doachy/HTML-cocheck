2c94d5b8772f05ab689d39c48e79c3ccb38707da
doachy <69197094+doachy@users.noreply.github.com>
2021-01-30 14:55:53 +0900

    Delete html directory

--- html/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
